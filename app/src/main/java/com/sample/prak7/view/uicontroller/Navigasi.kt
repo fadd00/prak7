4360195394c41780eba304b2d84bb814faba26d1
fadd00 <andhikahutama9@gmail.com>
2025-11-20 17:45:48 +0700

    feat: Add navigation functionality to return to Formulir from Detail

--- app/src/main/java/com/sample/prak7/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/sample/prak7/view/uicontroller/Navigasi.kt
@@ -1,5 +1,23 @@
 package com.sample.prak7.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.layout.padding
+import com.sample.prak7.model.DataJK.JenisK
+import com.sample.prak7.view.FormSiswa
+import com.sample.prak7.view.TampilSiswa
+import com.sample.prak7.viewmodel.SiswaViewModel
+
 enum class Navigasi {
     Formulir,
     Detail
@@ -37,3 +55,8 @@ fun SiswaApp(
             }
         }
     }
+}
+
+private fun cancelAndBackToFormulir(navController: NavHostController) {
+    navController.popBackStack(Navigasi.Formulir.name, inclusive = false)
+}
