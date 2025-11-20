0623d9c77abba453f842174e5e63070f09cba07e
fadd00 <andhikahutama9@gmail.com>
2025-11-20 17:44:59 +0700

    feat: Implement SiswaApp composable with navigation between Formulir and Detail

--- app/src/main/java/com/sample/prak7/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/sample/prak7/view/uicontroller/Navigasi.kt
@@ -4,3 +4,36 @@ enum class Navigasi {
     Formulir,
     Detail
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ){
+            composable(route = Navigasi.Formulir.name){
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = JenisK.map { id -> konteks.resources.getString(id) },
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilSiswa(
+                    statusUiSiswa = uiState.value,
+                    onBackButtonClicked = { cancelAndBackToFormulir(navController) }
+                )
+            }
+        }
+    }
